f7572d9e5250aee1296bef68de21409d27a10992
samozy <stan@mozy.org>
2025-07-15 17:58:32 -0700

    updated

--- README.md
+++ README.md
@@ -3,14 +3,10 @@ Roku SoundBridge Radio VFD Display library/wrapper in Go
 
 ToDo:
 ```
-func sb_disconnect() {}
 func sb_sync() {} // Keep-Alive?
 
 // Graphics Functions
-func point(int x, int y) {}
-func line() {}
-func square(int x_1, int y_1, int x_2, int y_2, bool filled){}
-func interleaving_lines() {
+func anim_interleaving_lines() {
 	// | ||| | ||| | ||| this kinda stuff. lines flip back and forth between on and off
 }
 
--- sb_lib.go
+++ sb_lib.go
@@ -1,4 +1,4 @@
-// v0.06
+// v0.08
 // 07/09/25
 // by sayzenberg
 
@@ -21,6 +21,10 @@ import (
 
 var sb_conn net.Dialer
 
+// IIRC?
+var MAX_X = 279
+var MAX_Y = 15 // ????
+
 func main() {
 	fmt.Println("Roku SoundBridge GFX Library")
 
@@ -30,6 +34,25 @@ func main() {
 	
 	// Draw
 	print_text("hello!", sb_conn)
+
+	// Clear
+	clear_screen(sb_conn)
+
+	// Line
+	draw_line(0, 0, 20, 20, sb_conn)
+
+	// Some tests
+	clear_screen(sb_conn)
+	draw_point(0, 0, sb_conn)
+        draw_point(0, MAX_Y, sb_conn)
+        time.Sleep(1 * time.Second)
+        draw_point(MAX_X, 0, sb_conn)
+        draw_point(MAX_X, MAX_Y, sb_conn)
+        time.Sleep(1 * time.Second)
+
+	// Square
+	// Args: Filled Rectangle, X, Y, Width, Height, conn ID
+	draw_rectangle(true, 10, 0, 10, 10, sb_conn)
 
 	// Exit
 	sb_disconnect(sb_conn)
@@ -55,8 +78,35 @@ func sb_disconnect(butts net.Conn) () {
 	butts.Close()
 }
 
+func clear_screen(butts net.Conn) {
+	fmt.Fprintf(butts, "clear\r\n")
+	time.Sleep(1 * time.Second)
+}
+
 func print_text(text string, butts net.Conn) () {
 	fmt.Println("Sending sketch")
 	fmt.Fprintf(butts, "text 5 5 %s\r\n", text)
 	time.Sleep(1 * time.Second)
 }
+
+func draw_line(x1 int, y1 int, x2 int, y2 int, butts net.Conn) {
+	fmt.Fprintf(butts, "line %d %d %d %d\r\n", x1, y1, x2, y2)
+	time.Sleep(1 * time.Second)
+}
+
+func draw_point(x int, y int, butts net.Conn) () {
+        fmt.Fprintf(butts, "point %d %d\r\n", x, y)
+        time.Sleep(1 * time.Second)
+}
+
+func draw_rectangle(filled bool, x int, y int, width int, height int, butts net.Conn) {
+	var tool string
+	if filled {
+		tool = "rect"
+	} else {
+		tool = "framerect"
+	}
+	fmt.Fprintf(butts, "%s %d %d %d %d\r\n", tool, x, y, width, height)
+	time.Sleep(1 * time.Second)
+}
+
